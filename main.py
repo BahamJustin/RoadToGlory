a7f89c3d08ec1e34d33580c90152f114ffca7844
BahamJustin <bahamjustin@outlook.com>
2022-10-18 19:47:08 -0500

    new branch

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 import random
 
-# def calculateScore():
-#     homeScore = str(7 * random.randint(0,5))
+homeTeam = 'New Orleans Saints'
+awayTeam = 'Atlanta Falcons'
+
+homeWins = 0
+homeLosses = 0
+homeTies = 0
+awayWins = 0
+awayLosses = 0
+awayTies = 0  
+
+homeRecord = f"{homeWins}/{homeLosses}/{homeTies}"
+awayRecord = f"{awayWins}/{awayLosses}/{awayTies}"
 
 def simulateGame():
-
-    homeTeam = 'New Orleans Saints'
-    awayTeam = 'Atlanta Falcons'
-
     homeScore = 7 * random.randint(0,5)
     homeScoreStr = str(homeScore)
     awayScore = 7 * random.randint(0,5)
     awayScoreStr = str(awayScore)
-    
+
     print(homeTeam + ":" + homeScoreStr +
     """
     """ +
@@ -20,9 +26,16 @@ def simulateGame():
 
     if homeScore == awayScore:
         print("It's a Tie!")
+        print(homeTeam, homeRecord)
+        print(awayTeam, awayRecord)
     elif homeScore > awayScore:
         print(homeTeam + " win!")
+        print(homeTeam, homeRecord)
+        print(awayTeam, awayRecord)
     else:
         print(awayTeam + " win!")
+        print(homeTeam, homeRecord)
+        print(awayTeam, awayRecord)
 
 simulateGame()
+
